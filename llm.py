3861f5df105b79e2faced397c2fda458cec06100
atokarev <atokarev@joytech.store>
2026-03-16 21:23:30 +0500

    fix build

--- llm.py
+++ llm.py
@@ -15,6 +15,8 @@ client = Groq(
 )
 
 cache: dict[str, list[str]] = {}
+model = "llama-3.1-8b-instant"
+system = "Ты саунд продюсер тебя зовут Начальник или @antonlamma_bot, ты разбираешься в ключевых вещах связанных с музыкой, знаешь как правильно сводить и делать ее. Ты находишься в чате с @killmeluther - продюсер зовут Паша делает треки в составе группы lamma, @soldier21 - Никита репер под ником waltyboy немного странный, @augkgb - Ринат репер под ником aughost(август) семьянин взрослый самостоятельный человек. Общаешься как обычный человек. Если считаешь что нужно принять участие в дискуссии то отправляй в конце isAnswer:true иначе isAnswer:false"
 
 
 def add_message(username: str, message: str):
@@ -35,12 +37,12 @@ def parse_message(message_res: str) -> tuple[str, bool]:
     return text, "isAnswer:true" in message_res
 
 
-def generation_message(username: str, message: str, history: str, default_answer: bool) -> str | None:
+def generation_message_chat(username: str, message: str, history: str, default_answer: bool) -> str | None:
     chat_history: list[ChatCompletionMessageParam] = [
         cast(ChatCompletionMessageParam, {
             "role": "system",
-            "content": """Ты саунд продюсер тебя зовут Начальник или @antonlamma_bot, ты разбираешься в ключевых вещах связанных с музыкой, знаешь как правильно сводить и делать ее. Ты находишься в чате с @killmeluther - продюсер зовут Паша делает треки в составе группы lamma, @soldier21 - Никита репер под ником waltyboy немного странный, @augkgb - Ринат репер под ником aughost(август) семьянин взрослый самостоятельный человек. Общаешься как обычный человек. Если считаешь что нужно принять участие в дискуссии то отправляй в конце isAnswer:true иначе isAnswer:false
-
+            "content": f"""
+    ${system}
     История чата:
     """ + history
         }),
@@ -51,7 +53,7 @@ def generation_message(username: str, message: str, history: str, default_answer
     ]
 
     completion = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model=model,
         messages=chat_history
     )
 
@@ -65,14 +67,36 @@ def generation_message(username: str, message: str, history: str, default_answer
         return None
 
 
+def generation_message():
+    messages: list[ChatCompletionMessageParam] = [cast(ChatCompletionMessageParam, {
+        "role": "system",
+        "content": f"${system}"
+    }), cast(ChatCompletionMessageParam, {
+        "role": "user",
+        "content": f"Назови какой нибудь факт как лучше всего сводить или писать музыку или сочинять"
+    })]
+
+    completion = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0.9
+    )
+
+    message_res = completion.choices[0].message.content
+    return message_res
+
+
 async def client_model_handler(message: str, username: str | None = None) -> str | None:
     add_message(username, message)
     history = "".join(cache.get(date.today().isoformat(), []))
 
-    if "@antonlamma_bot" in message:
-        return generation_message(username, message, history, True)
-
+    if message == "/sound":
+        return generation_message()
+    
     if message == "/history":
         return f"${username} запросил: ${history}"
 
-    return generation_message(username, message, history, False)
+    if "@antonlamma_bot" in message:
+        return generation_message_chat(username, message, history, True)
+
+    return generation_message_chat(username, message, history, False)
