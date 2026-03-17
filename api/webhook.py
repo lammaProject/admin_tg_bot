ca2e3ac94803b55f8c288e9e38a1c184a0cd49d5
atokarev <atokarev@joytech.store>
2026-03-17 15:44:19 +0500

    fix build

--- api/webhook.py
+++ api/webhook.py
@@ -54,7 +54,7 @@ async def process_update(update_data: dict):
 
     @dp.message()
     async def message_handler(message: types.Message):
-        if message.caption:
+        if message.document:
             await message.reply("TEST")
         if message.sticker:
             text = await analyze_file(message.sticker, bot)
