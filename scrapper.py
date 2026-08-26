f13bfd5b15b68707a2ded09ce0206bec21ce0e0f
lammaProject <anton999@bk.ru>
2026-08-26 08:12:38 +0500

    fix yandex

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
 from release_parser import get_yesterday, fetch_yesterdays_releases, format_releases_message, split_telegram_message
@@ -11,27 +12,25 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 
+def is_friday():
+    return datetime.now().weekday() == 4
+
+
 async def scrapper_test():
     if not BOT_TOKEN:
         raise RuntimeError("BOT_TOKEN is not set")
     if not CHAT_ID:
         raise RuntimeError("CHAT_ID is not set")
 
+    if not is_friday():
+        return
+
     bot = Bot(token=BOT_TOKEN)
 
     try:
-
-        print('start')
-        target_date = get_yesterday()
-        print(target_date)
-        releases = fetch_yesterdays_releases(
+        text = fetch_yesterdays_releases(
         )
-        total_count = len(releases)
-        text = format_releases_message(releases, target_date, total_count=total_count)
-        print(text)
-
-        for message in split_telegram_message(text):
-            await bot.send_message(CHAT_ID, message)
+        await bot.send_message(CHAT_ID, text)
 
 
     finally:
