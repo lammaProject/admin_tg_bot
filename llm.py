007764eafff84363d3592b44fb9720d086eb9410
lammaProject <anton999@bk.ru>
2026-09-10 23:33:00 +0500

    fix model

--- llm.py
+++ llm.py
@@ -31,7 +31,7 @@ client_genai = genai.Client(
 )
 client_redis = redis.Redis.from_url(REDIS_URL)
 
-model_groq = "qwen/qwen3.6-27b"
+model_groq = "openai/gpt-oss-20b"
 
 models_genai = [
     "gemini-3-flash-preview",
