da574fd74ceafe751ae09d8fedc63a00910476b4
lammaProject <anton999@bk.ru>
2026-09-10 22:53:37 +0500

    change model

--- llm.py
+++ llm.py
@@ -31,7 +31,7 @@ client_genai = genai.Client(
 )
 client_redis = redis.Redis.from_url(REDIS_URL)
 
-model_groq = "llama-3.1-8b-instant"
+model_groq = "qwen/qwen3.6-27b"
 
 models_genai = [
     "gemini-3-flash-preview",
